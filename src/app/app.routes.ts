df2fe169cdcaebc36d355691564fa759768e2115
cr9169 <bar.gabay.home@gmail.com>
2025-02-11 13:33:36 +0200

    feat: add routing for products and a PageNotFound component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { ProductsCatalogComponent } from './products/components/products-catalog/products-catalog.component';
+import { ProductDetailsComponent } from './products/components/product-details/product-details.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    children: [
+      {
+        path: '',
+        component: ProductsCatalogComponent,
+      },
+      {
+        path: ':id',
+        component: ProductDetailsComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
