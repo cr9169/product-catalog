823c869aad03dd3aad5a2ccf3de03414eeb446f3
cr9169 <bar.gabay.home@gmail.com>
2025-02-11 18:03:07 +0200

    feat: update product details component with new layout and routing, add Angular Material and Lucide icons

--- src/app/products/components/product-details/product-details.component.ts
+++ src/app/products/components/product-details/product-details.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { Product } from '../../models/product.model';
+import { map, Observable, switchMap } from 'rxjs';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import { Box, LucideAngularModule, Star } from 'lucide-angular';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [AsyncPipe, LucideAngularModule, RouterLink, CurrencyPipe],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent {
+  product$!: Observable<Product>;
 
+  readonly StarIcon = Star;
+  readonly BoxIcon = Box;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.product$ = this.route.paramMap.pipe(
+      map((params) => Number(params.get('id'))),
+      switchMap((id) => this.productService.getProductById(id))
+    );
+  }
 }
