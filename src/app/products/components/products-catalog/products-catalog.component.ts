e364100c21164c830988e94731ff2a5b11589af4
cr9169 <bar.gabay.home@gmail.com>
2025-02-11 20:59:59 +0200

    feat: enhance products catalog with filtering functionality and layout improvements

--- src/app/products/components/products-catalog/products-catalog.component.ts
+++ src/app/products/components/products-catalog/products-catalog.component.ts
@@ -2,21 +2,30 @@ import { Component } from '@angular/core';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductService } from '../../../services/product.service';
 import { Product } from '../../models/product.model';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { ProductsFilterComponent } from './products-filter/products-filter.component';
 
 @Component({
   selector: 'app-products-catalog',
-  imports: [ProductCardComponent, AsyncPipe],
+  imports: [ProductCardComponent, ProductsFilterComponent],
   templateUrl: './products-catalog.component.html',
   styleUrl: './products-catalog.component.scss',
 })
 export class ProductsCatalogComponent {
-  products$!: Observable<Product[]>;
+  products!: Product[];
+  filteredProducts!: Product[];
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.products$ = this.productService.getAllProducts();
+    this.productService.getAllProducts().subscribe((products) => {
+      this.products = products;
+      this.filteredProducts = products;
+    });
+  }
+
+  filterProducts(category: string) {
+    this.filteredProducts = category
+      ? this.products.filter((p) => p.category === category)
+      : this.products;
   }
 }
--- src/app/products/components/products-catalog/products-filter/products-filter.component.ts
+++ src/app/products/components/products-catalog/products-filter/products-filter.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../models/product.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products-filter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './products-filter.component.html',
-  styleUrl: './products-filter.component.scss'
+  styleUrl: './products-filter.component.scss',
 })
 export class ProductsFilterComponent {
+  @Input()
+  products!: Product[];
 
+  @Output()
+  categorySelected = new EventEmitter<string>();
+
+  selectedCategory: string = '';
+
+  get uniqueCategories() {
+    return [...new Set(this.products.map((product) => product.category))];
+  }
+
+  onSelectCategory() {
+    this.categorySelected.emit(this.selectedCategory);
+  }
 }
