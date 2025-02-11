ddf340c074ea8baec2c71d67ea33cc7d8cd3fdee
cr9169 <bar.gabay.home@gmail.com>
2025-02-11 13:17:53 +0200

    feat: add ProductDetails, ProductCard, ProductsCatalog, ProductsFilter components, and Product model file

---
+++ src/app/products/components/products-catalog/products-catalog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-catalog',
+  imports: [],
+  templateUrl: './products-catalog.component.html',
+  styleUrl: './products-catalog.component.scss'
+})
+export class ProductsCatalogComponent {
+
+}
---
+++ src/app/products/components/products-catalog/products-filter/products-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-filter',
+  imports: [],
+  templateUrl: './products-filter.component.html',
+  styleUrl: './products-filter.component.scss'
+})
+export class ProductsFilterComponent {
+
+}
